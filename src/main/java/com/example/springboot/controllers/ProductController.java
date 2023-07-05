005eda687266594d5133af0448542bd2f8c513b1
Arthur <arthur.yeshua@hotmail.com>
2023-07-05 09:46:20 -0300

    Criação método POST

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -1,15 +1,32 @@
 package com.example.springboot.controllers;
 
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
 public class ProductController {
 
     private final ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel); //converte o productRecordDto para o productModel
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 
 
 }
